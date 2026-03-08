6efe1bbb7dc8130fc0babf42184394fe334df988
Mint-Shock <mintshockyt+github@gmail.com>
2026-03-08 11:05:38 +0100

    Added css reset and 11ty navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
 	// Copy static assets
 	eleventyConfig.addPassthroughCopy('src/assets');
+
+	// Add navigation plugin
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
 	// Set input directory
 	return {
