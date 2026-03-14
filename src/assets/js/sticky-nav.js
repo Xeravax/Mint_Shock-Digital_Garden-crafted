b91f7be502ab4b62095f94e779bc0b5743f1bb13
Mint-Shock <mintshockyt+github@gmail.com>
2026-03-14 01:50:07 +0100

    Adjusted Background, nav sticky and Subnavs. Subnavs are actually usable now Also bg is slightly more Transparrent but the blobs behind are more blurry

--- src/assets/js/sticky-nav.js
+++ src/assets/js/sticky-nav.js
@@ -1,9 +1,13 @@
 // Sticky navigation
+const nav = document.getElementById('mainNav');
+const contentWrap = document.getElementById('mainWrap');
+
 window.addEventListener('scroll', function() {
-    const nav = document.getElementById('mainNav');
     if (window.scrollY > 0) {
         nav.classList.add('sticky');
+        contentWrap.style.paddingTop = nav.offsetHeight + 'px';
     } else {
         nav.classList.remove('sticky');
+        contentWrap.style.paddingTop = '';
     }
 });
